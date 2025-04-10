97a7d7ca7567d4d23435c4868f97c318d66fb082
JorgeArevalo-hub <jorgearevalomoran@gmail.com>
2025-04-10 16:52:03 +0000

    finished todolist

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [task, setTask] = useState("");
+	const [store, setStore] = useState([]);
+
+	const handleChange = (e) => {
+
+		setTask(e.target.value);
+
+	}
+	const handleSubmit = (e) => {
+
+		e.preventDefault();
+		setStore([...store, {task}])
+		setTask("")
+
+	}
+
+	const handleDelete = (index) => {
+		let del = store.filter((el,i) => i != index)
+		setStore(del)
+	}
+	
+
+	return (
+		<div>
+			<h1 className="text-center p-3">My Todolist with React</h1>
+			<form className="justificado" onSubmit={handleSubmit}>
+				<input className="formato justificado" type="text" value={task} onChange={handleChange} placeholder="What needs to be done?"/>
+				<input type="submit" hidden/>
+			</form>
+			<ul className="lista justificado">
+				{store.map((el,i) => <li className="formato justificado" key={i}>{el.task} <i onClick={e=>handleDelete(i)} className="fa-solid fa-xmark cruz"></i></li>)}
+			</ul>
+			<div className="formatoParrafo justificado">
+			<p className="parrafo">Left {store.length} items</p>
+			</div>
 		</div>
 	);
 };
